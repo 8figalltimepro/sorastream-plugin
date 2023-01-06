28d8b7ce5dd5961345370f1847314297f4678ff0
8figalltimepro <74225734+8figalltimepro@users.noreply.github.com>
2023-01-06 21:36:42 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/8figalltimepro/sorastream-plugin/")
 
-        authors = listOf("8Figalltimepro")
+        authors = listOf("8Figalltimeprhjjo")
     }
 
     android {
